d405822dbe9185bf5baf540ea415df75bfe0e635
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-08-24 16:52:30 +0530

    [Devesh]: Added simple linkedlist which is created node for linkedlist

--- src/com/datastructurell/LinkedListRunner.java
+++ src/com/datastructurell/LinkedListRunner.java
@@ -1,8 +1,14 @@
 package com.datastructurell;
 import java.util.LinkedList;
 
+import com.linkedlist.MyNode;
+
 public class LinkedListRunner {
 	public static void main(String[] args) {
+		
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
 		
 		LinkedList myLinked = new LinkedList();
 		
